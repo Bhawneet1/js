d73c684847b78955b721a947b89d6ca565a8de4e
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2024-08-17 06:55:16 +0000

    datatypes

---
+++ 01_basics/Second.js
@@ -0,0 +1,5 @@
+"use strict";//treat all JS code as newer version
+// alert("Hello") we are using node js not browser therefore error aega
+// alert(3+3)
+console.log(3+3)
+//code readability should be high
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+let name="Bhawneet "//string
+let age=20//integer
+let isloggedIn=false//boolean
+let g=null
+
+
+//Primitive datatypes
+//number=> 2^53(range)
+//bigInt(very big number as stock market)
+//string(single or double quotes)
+//boolean(yes or no,true/false)
+//null(stand alone value)(means ki empty hai par undefined nahi hai)
+//undefine(if no value assigned)
+//symbol(to find unique component )
+
+
+
+console.log(typeof "Bhawneet")
+console.log(typeof isloggedIn)
+console.log(typeof null)
+console.log(typeof undefined)
