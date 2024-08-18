34579bddfbd5878b98fa9c62e72ec7a58ddc089d
Bhawneet Singh <136002853+Bhawneet1@users.noreply.github.com>
2024-08-18 03:47:03 +0000

    Operations

---
+++ 01_basics/Operations.js
@@ -0,0 +1,39 @@
+let value=3;
+let negvalue=-value;//value ka negetive ho jaega
+console.log(negvalue)
+
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);//power
+console.log(2/3);
+console.log(2%3);
+
+let str1="hello"
+let str2="Bhawneet"
+let str3=str1+" "+str2//concatinate strings
+console.log(str3)
+
+console.log(1+"2")//12
+console.log("1"+2);//12
+console.log("1"+"2");//12
+console.log("1"+2+2)//122
+console.log(1+2+"2")//32
+
+
+console.log(+true)//1(+ convert it to number)
+
+// console.log(true+)//error
+console.log(+"")//0
+
+let num1,num2,valueInNumber3
+
+num1=num2=num3=2+2 //not good readability
+
+console.table([num1,num2,num3])
+
+
+let gameCounter=100;
+gameCounter++;
+console.log(gameCounter)
